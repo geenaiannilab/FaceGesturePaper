3b955bd7eeac4524fc69809ecbd49a8813dab025
geenaiannilab <35614555+geenaiannilab@users.noreply.github.com>
2025-09-18 07:24:31 -0400

    plotting figures 4C, 4D for publication

--- Figure3/plotCorrStructureAcrossExpressions.m
+++ Figure3/plotCorrStructureAcrossExpressions.m
@@ -17,12 +17,12 @@ close all; clear all
 set(0,'defaultAxesFontSize', 24); % bc im blind 
 set(0,'defaultAxesFontWeight', 'bold'); 
 
-date = '171128';
-subject ='Thor';
-workdir = (['/Users/geena/Dropbox/PhD/SUAinfo/' subject '_' date '/CorrMatricesV2']);
-regions = {'All'};
+workdir = '/Users/geena/Documents/MATLAB/projects/FacialGesturesPaperCode/FaceGesturePaper/Figure3/matFiles/CorrelationMatrices';
+regions = {'S1','M1','PMv','M3','All'};
 bhvs2plot = [1 2 4];
 bhvStrings = {'Threat','Lipsmack','Chew'};
+
+data = load('combinedData_R2_pairwiseNeuralCorrelations.mat');
 
 % region colormap
 cmap1 = [0.4940 0.1840 0.5560	
@@ -31,65 +31,15 @@ cmap1 = [0.4940 0.1840 0.5560
     0.9290 0.6940 0.1250
     0.5 0.5 0.5];
 
-% for each region, plot the self-sorted & unified sorting signal
-% correlations, per behavior 
-%%
-for rr = 1:length(regions)
-    data = load([workdir '/' regions{rr} '.mat']);
-    
-    taxis = data.taxis;
-
-     % plot pairwise correlations
-    r2_1v2 = rsquare(data.prCorr{:,1}', data.prCorr{:,2}');
-    r2_1v3 = rsquare(data.prCorr{:,1}', data.prCorr{:,3}');
-    r2_2v3 = rsquare(data.prCorr{:,2}', data.prCorr{:,3}');
-
-    disp(['Region is ' regions{rr}])
-    disp([regions{rr} ': Mean R2 = ' num2str(mean([r2_1v2 r2_1v3 r2_2v3]))])
-
-    fig = figure('units','normalized','outerposition',[0 0 1 1]);
-    fig.Position = [0    0.4399    1.0000    0.4409];
-
-    subplot 131; scatter(data.prCorr{:,1},data.prCorr{:,2},'MarkerFaceColor','k','MarkerEdgeColor','k',...
-    'MarkerFaceAlpha',.2,'MarkerEdgeAlpha',.2); 
-    xlabel('Pairwise Correlation (Threat)','FontSize',20); ylabel('Pairwise Correlation (Lipsmack)','FontSize',20); xlim([-1 1]); ylim([-1 1 ]);
-    axes = gca; axes.XTick = [-1 0 1]; axes.YTick = [-1 0 1];
-    %text(-1,-1,['R2 = ' num2str( r2_1v2 )]);
-    axes.FontWeight = 'bold';
-    disp(['R2 = ' num2str( r2_1v2 )]);
-
-    subplot 132; scatter(data.prCorr{:,1},data.prCorr{:,3},'MarkerFaceColor','k','MarkerEdgeColor','k',...
-    'MarkerFaceAlpha',.2,'MarkerEdgeAlpha',.2); 
-    xlabel('Pairwise Correlation (Threat)','FontSize',20); ylabel('Pairwise Correlation (Chew)','FontSize',20);xlim([-1 1]); ylim([-1 1 ]);
-    axes = gca; axes.XTick = [-1 0 1]; axes.YTick = [-1 0 1];
-    %text(-1,-1,['R2 = ' num2str( r2_1v3 )]);
-    axes.FontWeight = 'bold';
-    disp(['R2 = ' num2str( r2_1v3 )]);
-
-    subplot 133; scatter(data.prCorr{:,2},data.prCorr{:,3},'MarkerFaceColor','k','MarkerEdgeColor','k',...
-        'MarkerFaceAlpha',.2,'MarkerEdgeAlpha',.2);
-    xlabel('Pairwise Correlation (Lipsmack)','FontSize',20); ylabel('Pairwise Correlation (Chew)','FontSize',20);xlim([-1 1]); ylim([-1 1 ]);
-    axes = gca; axes.XTick = [-1 0 1]; axes.YTick = [-1 0 1];
-    axes.FontWeight = 'bold';
-    %text(-1,-1,['R2 = ' num2str( r2_2v3 )]);
-    disp(['R2 = ' num2str( r2_2v3 )]);
-
-    sgtitle(['Neuron Pairwise Correlations: ' regions{rr}],'FontSize',36,'FontWeight','bold');
-
-    %plotFig = gcf;
-    %savefig(plotFig, ['~/Dropbox/PhD/Manuscript/subFigurePDFs/' regions{rr} '_' date '_neuralSignalCorr_scatter.fig']);
-    %exportgraphics(plotFig, ['~/Dropbox/PhD/Manuscript/subFigurePDFs/' regions{rr} '_' date '_neuralSignalCorr_scatter.pdf'], 'ContentType', 'vector');
-
-end
-
-% correlation matrices 
+%% Fig 4D, top and bottom rows 
+%% for each region, plot self-sorted and unified sorted, neural correlation matrices 
 for rr = 1:length(regions)
     
     data = load([workdir '/' regions{rr} '.mat']);
 
    clear axes;
 
-    fig = figure; fig.Position =  [ 476          97        1037         769];
+    fig1 = figure; fig1.Position =  [ 476          97        1037         769];
     ha = tight_subplot(2,3,[.05 .05], [0.05 0.1]);
 
     for bhv = 1:length(bhvs2plot)
@@ -124,48 +74,77 @@ for rr = 1:length(regions)
     c.Position = [0.96 0.09 0.01 0.8];
     c.Ticks = [0 1 2];
     c.FontWeight= 'bold';
+    c.Label.String = '1 - Pearsons R';
 
-    %savefig(fig, ['~/Dropbox/PhD/Manuscript/subFigurePDFs/' regions{rr} '_' date '_neuralSignalCorr.fig']);
-    %exportgraphics(fig, ['~/Dropbox/PhD/Manuscript/subFigurePDFs/' regions{rr} '_' date '_neuralSignalCorr.pdf'], 'ContentType', 'vector');
 end % end regions
-%%
 
-% Plot the R^2 
-thorData = load('/Users/geena/Dropbox/PhD/SUAinfo/Thor_R2ValsAcrossDays.mat');
-barneyData = load('/Users/geena/Dropbox/PhD/SUAinfo/Barney_R2ValsAcrossDays.mat');
+%% for each region, plot R^2 (neural correlations during one gesture vs. during another, pairwise comparisons) 
+%% supplemental 
+for rr = 1:length(regions)
+    data = load([workdir '/' regions{rr} '.mat']);
+    
+    taxis = data.taxis;
 
-thorMeans_1v2 = mean(thorData.r2_1v2,1); 
-thorMeans_1v3 = mean(thorData.r2_1v3,1);
-thorMeans_2v3 = mean(thorData.r2_2v3,1);
+     % plot pairwise correlations
+    r2_1v2 = rsquare(data.prCorr{:,1}', data.prCorr{:,2}');
+    r2_1v3 = rsquare(data.prCorr{:,1}', data.prCorr{:,3}');
+    r2_2v3 = rsquare(data.prCorr{:,2}', data.prCorr{:,3}');
 
-barneyMeans_1v2 = mean(barneyData.r2_1v2,1);
-barneyMeans_1v3 = mean(barneyData.r2_1v3,1);
-barneyMeans_2v3 = mean(barneyData.r2_2v3,1);
+    disp(['Region is ' regions{rr}])
+    disp([regions{rr} ': Mean R2 = ' num2str(mean([r2_1v2 r2_1v3 r2_2v3]))])
 
-combinedData_1v2 = [thorData.r2_1v2; barneyData.r2_1v2];
-combinedData_1v3 = [thorData.r2_1v3; barneyData.r2_1v3];
-combinedData_2v3 = [thorData.r2_2v3; barneyData.r2_2v3];
+    fig2 = figure('units','normalized','outerposition',[0 0 1 1]);
+    fig2.Position = [0    0.4399    1.0000    0.4409];
 
-combinedPval_1v2 = [thorData.p_1v2; barneyData.p_1v2];
-combinedPval_1v3 = [thorData.p_1v3; barneyData.p_1v3];
-combinedPval_2v3 = [thorData.p_2v3; barneyData.p_2v3];
+    subplot 131; scatter(data.prCorr{:,1},data.prCorr{:,2},'MarkerFaceColor','k','MarkerEdgeColor','k',...
+    'MarkerFaceAlpha',.2,'MarkerEdgeAlpha',.2); 
+    xlabel('Pairwise Correlation (Threat)','FontSize',20); ylabel('Pairwise Correlation (Lipsmack)','FontSize',20); xlim([-1 1]); ylim([-1 1 ]);
+    axes = gca; axes.XTick = [-1 0 1]; axes.YTick = [-1 0 1];
+    %text(-1,-1,['R2 = ' num2str( r2_1v2 )]);
+    axes.FontWeight = 'bold';
+    disp(['R2 = ' num2str( r2_1v2 )]);
 
-combinedMeans_1v2 = mean([thorData.r2_1v2 ; barneyData.r2_1v2],1);
-combinedMeans_1v3 = mean([thorData.r2_1v3 ; barneyData.r2_1v3],1);
-combinedMeans_2v3 = mean([thorData.r2_2v3 ; barneyData.r2_2v3],1);
+    subplot 132; scatter(data.prCorr{:,1},data.prCorr{:,3},'MarkerFaceColor','k','MarkerEdgeColor','k',...
+    'MarkerFaceAlpha',.2,'MarkerEdgeAlpha',.2); 
+    xlabel('Pairwise Correlation (Threat)','FontSize',20); ylabel('Pairwise Correlation (Chew)','FontSize',20);xlim([-1 1]); ylim([-1 1 ]);
+    axes = gca; axes.XTick = [-1 0 1]; axes.YTick = [-1 0 1];
+    %text(-1,-1,['R2 = ' num2str( r2_1v3 )]);
+    axes.FontWeight = 'bold';
+    disp(['R2 = ' num2str( r2_1v3 )]);
 
-combinedStd_1v2 = std([thorData.r2_1v2 ; barneyData.r2_1v2],[],1);
-combinedStd_1v3 = std([thorData.r2_1v3 ; barneyData.r2_1v3],[],1);
-combinedStd_2v3 = std([thorData.r2_2v3 ; barneyData.r2_2v3],[],1);
+    subplot 133; scatter(data.prCorr{:,2},data.prCorr{:,3},'MarkerFaceColor','k','MarkerEdgeColor','k',...
+        'MarkerFaceAlpha',.2,'MarkerEdgeAlpha',.2);
+    xlabel('Pairwise Correlation (Lipsmack)','FontSize',20); ylabel('Pairwise Correlation (Chew)','FontSize',20);xlim([-1 1]); ylim([-1 1 ]);
+    axes = gca; axes.XTick = [-1 0 1]; axes.YTick = [-1 0 1];
+    axes.FontWeight = 'bold';
+    %text(-1,-1,['R2 = ' num2str( r2_2v3 )]);
+    disp(['R2 = ' num2str( r2_2v3 )]);
 
-combinedMeansAllBhv = mean([thorData.r2_1v2 ; thorData.r2_1v3; thorData.r2_2v3; barneyData.r2_1v2 ;barneyData.r2_1v3; barneyData.r2_2v3 ],1);
-combinedStdAllBhv = std([thorData.r2_1v2 ; thorData.r2_1v3; thorData.r2_2v3; barneyData.r2_1v2 ;barneyData.r2_1v3; barneyData.r2_2v3 ],[],1);
+    sgtitle(['Neuron Pairwise Correlations: ' regions{rr}],'FontSize',36,'FontWeight','bold');
 
-combinedMeansAllBhvAllRegions = mean([thorData.r2_1v2 ; thorData.r2_1v3; thorData.r2_2v3; barneyData.r2_1v2 ;barneyData.r2_1v3; barneyData.r2_2v3 ],'all');
-combinedStdAllBhvAllRegions = std([thorData.r2_1v2 ; thorData.r2_1v3; thorData.r2_2v3; barneyData.r2_1v2 ;barneyData.r2_1v3; barneyData.r2_2v3 ],[],'all');
+   
+end
 
 
-% Plot R2 for threat v lipsmack (bhv1v2)
+%% Plot the R^2 values across all days
+%% Fig 4C
+
+combinedData_1v2 = data.combinedData_1v2;
+combinedData_1v3 = data.combinedData_1v3;
+combinedData_2v3 = data.combinedData_2v3;
+
+combinedPval_1v2 = data.combinedPval_1v2;
+combinedPval_1v3 = data.combinedPval_1v3;
+combinedPval_2v3 = data.combinedPval_2v3;
+
+combinedMeansAllBhv = data.combinedMeansAllBhv;
+combinedStdAllBhv = data.combinedStdAllBhv;
+
+combinedMeansAllBhvAllRegions = data.combinedMeansAllBhvAllRegions;
+combinedStdAllBhvAllRegions = data.combinedStdAllBhvAllRegions;
+
+
+%% Plot R2 for threat v lipsmack (bhv1v2)
 figure; 
 counter = 1;
 
@@ -192,7 +171,7 @@ for dd = 1:size(combinedData_1v2,1)
     end
 end
 
-% Plot R2 for threat V chew (bhv1v3)
+%% Plot R2 for threat V chew (bhv1v3)
 counter = 1;
 
 for dd = 1:size(combinedData_1v3,1)
@@ -218,7 +197,7 @@ for dd = 1:size(combinedData_1v3,1)
     end
 end
 
-% Plot R2 for lipsmack V chew (bhv2v3)
+%% Plot R2 for lipsmack V chew (bhv2v3)
 
 counter = 1;
 
@@ -250,8 +229,8 @@ for rr = 1:length(regions)
     hold on 
 end
 hold off
-ylabel('R-Squared');
-title('Pairwise Neuron Correlations');
+ylabel('R^2');
+title('Average Pairwise Neuron Correlations, All Days');
 
 hold off; 
 axes = gca;
